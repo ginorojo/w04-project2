b99339c56fe2d795245a01cf171bf1aeb8f10b2a
ginorojo <ginorojoj@gmail.com>
2026-05-28 13:27:17 -0400

    camnbios

--- swagger.js
+++ swagger.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const swaggerAutogen = require('swagger-autogen')();
 
+const defaultBaseUrl = `http://localhost:${process.env.PORT || 3000}`;
+const configuredBaseUrl = process.env.RENDER_EXTERNAL_URL || process.env.HOST || defaultBaseUrl;
+const normalizedBaseUrl = /^[a-zA-Z][a-zA-Z\d+\-.]*:\/\//.test(configuredBaseUrl)
+    ? configuredBaseUrl
+    : `http://${configuredBaseUrl}`;
+const parsedBaseUrl = new URL(normalizedBaseUrl);
+
 const doc = {
     info: {
         title: 'Expense API',
         description: 'REST API with expense CRUD, validation, Swagger, and GitHub OAuth.'
     },
-    host: `localhost:${process.env.PORT || 3000}`,
-    schemes: ['http'],
+    host: parsedBaseUrl.host,
+    schemes: [parsedBaseUrl.protocol.replace(':', '')],
     consumes: ['application/json'],
     produces: ['application/json']
 };
